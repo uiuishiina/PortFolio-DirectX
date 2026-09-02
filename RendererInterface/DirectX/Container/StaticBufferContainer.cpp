535581386721c2000c450c5a9c4b91b242f138de
うい 椎名 <shiina04jbs10@outlook.com>
2026-09-02 13:08:10 +0900

    StaticBufferContainerクラス大幅変更

--- RendererInterface/DirectX/Container/StaticBufferContainer.cpp
+++ RendererInterface/DirectX/Container/StaticBufferContainer.cpp
@@ -1,20 +1,3 @@
 #include"StaticBufferContainer.h"
 
 using namespace render::dx12::container;
-
-///====================================================================
-/// 実行時処理関数
-///====================================================================
-
-//@brief	=== リソース取得関数 ===
-//@param	key		リソースと紐づけたキー
-//@return	リソースクラス参照
-[[nodiscard]] render::dx12::object::StaticBufferResource* StaticBufferContainer::get_resource(UINT key)const noexcept {
-
-    //  登録済みか確認
-    const auto it = buffer_map.find(key);
-    if (it == buffer_map.end()) {
-        return nullptr;
-    }
-    return it->second.get();
-}
--- RendererInterface/DirectX/Container/StaticBufferContainer.h
+++ RendererInterface/DirectX/Container/StaticBufferContainer.h
@@ -1,21 +1,15 @@
 #pragma once
 #include"../DirectXobject/StaticBufferResource.h"
-#include"StaticContainerBase.h"
-#include<unordered_map>
-#include<memory>
-#include<vector>
-#include<concepts>
+#include"UniqueptrKeyMap.h"
 
-///====================================================================
+/// <summary>
 /// 描画名前空間
-///====================================================================
-
+/// </summary>
 namespace render {
 
-	///====================================================================
+	/// <summary>
 	/// DirectX名前空間
-	///====================================================================
-
+	/// </summary>
 	namespace dx12 {
 
 		///====================================================================
@@ -29,120 +23,107 @@ namespace render {
 			concept FromStaticBuffer = std::derived_from<T, object::StaticBufferResource>;
 		};
 
-		///====================================================================
-		/// コンテナ名前空間
-		///====================================================================
 
+		/// <summary>
+		/// コンテナ名前空間
+		/// </summary>
 		namespace container {
+
+			/* ========== 描画バッファ設定用キー定義 ========== */
+
+			/// <summary>
+			/// ハンドル名前空間
+			/// </summary>
+			namespace handle {
+
+				/// <summary>
+				/// 描画バッファ設定用倫理側派生キー
+				/// </summary>
+				struct StaticBufferKey : public LogicalKey {
+
+					/// <summary>
+					/// コンストラクタ
+					/// </summary>
+					StaticBufferKey() = default;
+
+					/// <summary>
+					/// 引数付きコンストラクタ
+					/// </summary>
+					/// <param name="key">キーに入れる値</param>
+					explicit StaticBufferKey(std::uint32_t key) :
+						LogicalKey{ key } {}
+
+					/// <summary>
+					/// 引数付きコンストラクタ
+					/// </summary>
+					/// <param name="key">キーに入れる値</param>
+					explicit StaticBufferKey(const char* key_name) :
+						LogicalKey{ static_cast<std::uint32_t>(HandyItems::id::get_id::get_name_id<StaticBufferKey>(key_name)) } {}
+				};
+
+				/// <summary>
+				/// 描画バッファ設定用保存側派生キー
+				/// </summary>
+				struct StaticBufferEncodeKey : public EncodeKey {
+
+					/// <summary>
+					/// コンストラクタ
+					/// </summary>
+					StaticBufferEncodeKey() = default;
+				};
+			}
+
+
 
 			///====================================================================
 			/// StaticBufferContainer クラス
 			///====================================================================
 
 			//@brief	=== 初期作成描画バッファリソースコンテナクラス ===
-			class StaticBufferContainer final : public StaticContainerBase
+			class StaticBufferContainer final : public UniqueptrKeyMap<
+				handle::StaticBufferKey,
+				handle::StaticBufferEncodeKey,
+				object::StaticBufferResource
+			>
 			{
 			public:
-				///====================================================================
-				/// クラス設定
-				///====================================================================
+				/* ========== メンバー関数 ========== */
 
-				//コンストラクタ,デストラクタ
+				/// <summary>
+				/// コンストラクタ
+				/// </summary>
 				StaticBufferContainer() = default;
+
+				/// <summary>
+				/// デストラクタ
+				/// </summary>
 				~StaticBufferContainer() = default;
 
-				///====================================================================
-				/// Public メンバー関数
-				///====================================================================
+				/* ===== 追加関数 ===== */
 
-				//@brief	=== 初期作成描画バッファリソース登録関数 ===
-				//@param	key_name	登録するキーの名前
-				//@param	buffer		登録するバッファクラスインスタンス
-				//@return	登録の成否
+				/// <summary>
+				/// 描画バッファリソース登録関数
+				/// </summary>
+				/// <typeparam name="T">描画バッファ継承型</typeparam>
+				/// <param name="key">追加したい倫理側のキー</param>
+				/// <param name="buffer">設定する描画バッファ</param>
+				/// <returns>登録の成否</returns>
 				template<concepts::FromStaticBuffer T>
-				[[nodiscard]] bool register_buffer(const std::string& key_name, std::unique_ptr<T> buffer);
-
-				//@breif	=== 初期作成描画バッファリソース取得関数 ===
-				//@param	key		リソースと紐づけたキー
-				//@return	[ T ] 型にキャストしたバッファクラス参照
-				template<concepts::FromStaticBuffer T>
-				[[nodiscard]] T* get_buffer(UINT key)const noexcept;
-
-				//@breif	=== 初期作成描画バッファリソース取得オーバーロード関数 ===
-				//@param	key_name	リソースと紐づけたキーの名前
-				//@return	[ T ] 型にキャストしたバッファクラス参照
-				template<concepts::FromStaticBuffer T>
-				[[nodiscard]] T* get_buffer(const std::string& key_name)const noexcept;
-
-			private:
-				///====================================================================
-				/// Private メンバー関数
-				///====================================================================
-
-				//@brief	== 描画バッファ保存マップ ===
-				//@details	作成したバッファを保存するmap
-				std::unordered_map<UINT, std::unique_ptr<object::StaticBufferResource>> buffer_map{};
-
-				///====================================================================
-				/// Private メンバー関数
-				///====================================================================
-
-				//@brief	=== リソース取得関数 ===
-				//@param	key		リソースと紐づけたキー
-				//@return	リソースクラス参照
-				[[nodiscard]] object::StaticBufferResource* get_resource(UINT key)const noexcept;
+				[[nodiscard]] bool register_buffer(const handle::StaticBufferKey& key, std::unique_ptr<T> buffer);
 
 			};
 
-			//@brief	=== 初期作成描画バッファリソース登録関数 ===
-			//@param	key_name	登録するキーの名前
-			//@param	buffer		登録するバッファクラスインスタンス
-			//@return	登録の成否
+			/// <summary>
+			/// 描画バッファリソース登録関数
+			/// </summary>
+			/// <typeparam name="T">描画バッファ継承型</typeparam>
+			/// <param name="key">追加したい倫理側のキー</param>
+			/// <param name="buffer">設定する描画バッファ</param>
+			/// <returns>登録の成否</returns>
 			template<concepts::FromStaticBuffer T>
-			[[nodiscard]] bool StaticBufferContainer::register_buffer(const std::string& key_name,std::unique_ptr<T> buffer) {
-				
-				//  登録済みか確認
-				auto hash = get_hash_key(key_name);
-				if (hash.has_value()) {
-					return false;
-				}
+			[[nodiscard]] bool StaticBufferContainer::register_buffer(const handle::StaticBufferKey& key, std::unique_ptr<T> buffer) {
 
-				auto new_hash = allocate_hash(key_name);
-				buffer_map.emplace(new_hash, std::move(buffer));
-
-				return true;
-			}
-
-			//@breif	=== 初期作成描画バッファリソース取得関数 ===
-			//@param	key		リソースと紐づけたキー
-			//@return	[ T ] 型にキャストしたバッファクラス参照
-			template<concepts::FromStaticBuffer T>
-			[[nodiscard]] T* StaticBufferContainer::get_buffer(UINT key)const noexcept {
-
-				//	コンテナから取得
-				auto value = get_resource(key);
-				if (value == nullptr) {
-					return nullptr;
-				}
-
-				//	T型にキャスト
-				return dynamic_cast<T*>(value);
-			}
-
-			//@breif	=== 初期作成描画バッファリソース取得オーバーロード関数 ===
-			//@param	key_name	リソースと紐づけたキーの名前
-			//@return	[ T ] 型にキャストしたバッファクラス参照
-			template<concepts::FromStaticBuffer T>
-			[[nodiscard]] T* StaticBufferContainer::get_buffer(const std::string& key_name)const noexcept {
-				
-				//  登録されているか確認
-				auto hash = get_hash_key(key_name);
-				if (!hash.has_value()) {
-					return nullptr;
-				}
-
-				//	上の関数に処理を任せる
-				return get_buffer<T>(hash.value());
+				return add_value(key, std::move(buffer));
 			}
 		};
 	};
